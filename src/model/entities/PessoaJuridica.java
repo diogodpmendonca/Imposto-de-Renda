6324528d6f5e2c2642370afbdaa42f4a6bb84a15
diogodpmendonca <diogodpmendonca@gmail.com>
2018-12-27 10:08:47 -0200

    Criado Classe PessoaJurica extendida de Contribuinte

---
+++ src/model/entities/PessoaJuridica.java
@@ -0,0 +1,35 @@
+package model.entities;
+
+public class PessoaJuridica extends Contribuinte{
+	
+	public Integer qtdfuncionarios;
+
+	public PessoaJuridica(Integer qtdfuncionarios) {
+		super();
+		this.qtdfuncionarios = qtdfuncionarios;
+	}
+
+	public Integer getQtdfuncionarios() {
+		return qtdfuncionarios;
+	}
+
+	public void setQtdfuncionarios(Integer qtdfuncionarios) {
+		this.qtdfuncionarios = qtdfuncionarios;
+	}
+	
+	@Override
+	public Double impostoDevido() {
+		if(qtdfuncionarios > 10) {
+			return renda * 0.14;
+		}else {
+			return renda * 0.15;
+		}
+	}
+	
+	@Override
+	public String demosntrativo() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(nome+": $"+impostoDevido());
+		return sb.toString();
+	}
+}
